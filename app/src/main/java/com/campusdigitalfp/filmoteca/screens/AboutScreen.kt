ac5a9f1cf58ba6f04ecc0215dbd3d3e2965e0104
little-shiny <mim0xina@gmail.com>
2025-12-14 11:38:07 +0100

    Ejercicio 12 - TopAppBar Añadida TopAppBar en todas las pantallas /todo mostrar result_cancelled en filmEditScreen

--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/AboutScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/AboutScreen.kt
@@ -3,7 +3,6 @@ package com.campusdigitalfp.filmoteca.screens
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
