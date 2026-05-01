17e5e8deb505f000e72501be390821ce29b61b76
Cristina ❤️ <mim0xina@gmail.com>
2026-05-01 18:46:19 +0200

    Reescritura de common.kt estableciendo la barra superior y el menú desplegable. Se eliminan parámetros innecesarios que guardaban cual era la pantalla en la que se encontraba la barra superior pero era redundante. Se añaden multiples iconos para las acciones con sus content descriptions

--- app/src/main/java/com/campusdigitalfp/filmoteca/common/common.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/common/common.kt
@@ -1,12 +1,12 @@
 package com.campusdigitalfp.filmoteca.common
 
+import android.service.autofill.OnClickAction
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
@@ -14,100 +14,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.campusdigitalfp.filmoteca.R
 import com.campusdigitalfp.filmoteca.models.Film
+import com.campusdigitalfp.filmoteca.navigation.NavRoutes
+import com.campusdigitalfp.filmoteca.viewmodel.FilmViewModel
 
-// Funciones composables compartidas en toda la app
-
-// Barra superior
+/**
+ * Componente `BarraSuperior` que representa la barra superior de la app
+ * incluye navegación, menú desplegable y opciones para gestionar las peliculas
+ */
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun barraSuperior(
     navController: NavHostController,
-    esHome: Boolean = false,
     atras: Boolean = true,
     menu: Boolean = false,
-    onBackResult: (() -> Unit)? = null, //parámetro opcional que puede existir o no y solo se usa en filEditScreen
-    onAddFilm:  (() -> Unit)? = null, // Acción opcional para la accion al pulsar añadir película
-    onAbout: (() -> Unit)? = null, // Acción al pulsar acerca de
-    // Se utiliza (() -> Unit)? para que la barra sea reutilizable y sean funciones opcionales
-    //parametros para la seleccion multiple
-    isActionMode: MutableState<Boolean>? = null,
-    selectedFilms: SnapshotStateList<Film> = null, // permite coger datos de una lista observable y que puede ser de
-    // cualquier elmemento. no especifico film porque podría usarse para otro tipo de listas
-    onDeleteSelected: (() -> Unit)? = null
-
-
-
-
-
+    isActionMode: MutableState<Boolean> = mutableStateOf(false),
+    selectedFilms: MutableList<Film> = mutableListOf(),
+    viewModel: FilmViewModel = viewModel()
 ){
-    var expanded by remember { mutableStateOf(false) }
-    val actionModeActive = isActionMode?.value ==true
+    var expanded by remember { mutableStateOf(false) } // estado del menú desplegable
 
-    //variable que almacena el color de la barra dependiendo de si se está haciendo una selección múltiple
-    val appBarColor =
-        if (actionModeActive)
-            MaterialTheme.colorScheme.secondaryContainer
-        else
-            MaterialTheme.colorScheme.primaryContainer
-
-    // TopAppBar
     TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = appBarColor
+        colors = TopAppBarDefaults.CenterAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer, //Fondo de la barra
+            titleContentColor = MaterialTheme.colorScheme.primary // Color del texto del título
         ),
-
-        // Título
-        title = {
-            if(actionModeActive && selectedFilms != null){
-                //Se muestra el texto al hacer seleccion múltiple
-                Text("${selectedFilms.size} películas seleccionadas") // muestra el tamaño de la lista de los elementos
-            // seleccionados
-            }else{
-                if(esHome){
-                    Text("Filmoteca") // si no se hace sellección muestra el título si es la pantalla home
-                }else{ // si es cualquier otra pantalla muestra el icono para volver al home
-                    Box(
-                        modifier = Modifier.clickable {
-                            navController.navigate("FilmListScreen"){
-                                //al pulsar el icono volvemos a home
-                                popUpTo(0)
-                            }
-                        }
-                    ){
-                        Icon(
-                            painter = painterResource(id =R.drawable.film),
-                            contentDescription = "Home",
-                            tint = Color.Unspecified,
-                            modifier = Modifier
-                                .size(40.dp)
-                        )
-                    }
-                }
-            }
+        title =  {
+            Text(text = "Filmoteca")
         },
-        // Flecha atrás opcional (solo en pantallas que no sean home)
         navigationIcon = {
-            //Cancelar selección
-            if(actionModeActive){
-                IconButton(onClick = {
-                    selectedFilms?.clear()
-                    isActionMode?.value = false
-                }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Cancelar selección"
-                    )
-                }
-                //Volver atrás
-            } else if (atras && !esHome){
-                IconButton( onClick = {
-                    onBackResult?.invoke()
-                    navController.popBackStack()
-                }) {
+            if(atras){
+                //Muestra el botón de retroceso
+                IconButton(onClick = { navController.popBackStack()}) {
                     Icon(
                         imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "Atrás"
@@ -115,51 +57,73 @@ fun barraSuperior(
                 }
             }
         },
-
-        //Acciones parte derecha de la barra superior
         actions = {
-            if(actionModeActive){ // Ponemos icono de borrar para borrar las películas seleccionadas
-                IconButton(onClick = {onDeleteSelected?.invoke() }) {
+            if(!atras){
+                // si atras es false muestra el menú desplegable
+                IconButton(onClick = {expanded = true}) {
                     Icon(
-                        imageVector = Icons.Default.Delete,
-                        contentDescription = "Eliminar seleccionados"
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "Menú desplegable"
                     )
                 }
-            }
-
-            //Menú derecho
-            if (menu) {
-                IconButton(
-                    //Ocultamos el icono del menú si hay selección múltiple
-                    onClick = { if (!actionModeActive) expanded = true },
-                    enabled = !actionModeActive
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = {expanded = false}
+                    // Cierra el menú cuando se toca fuera de el
                 ) {
-                    Icon(
-                        imageVector = Icons.Filled.MoreVert,
-                        contentDescription = "Menú"
+                    // opción para añadir una nueva película
+                    DropdownMenuItem(
+                        onClick = {
+                            expanded = false
+                            navController.navigate(NavRoutes.NEW.abreviatura)
+                        },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Filled.Add,
+                                contentDescription = "Añadir película",
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        },
+                        text = { Text("Nuevo")}
+                    )
+                    // Opción para ir a la pantalla "Acerca de"
+                    DropdownMenuItem(
+                        onClick = {
+                            expanded = false
+                            viewModel.addExampleFilms() // agrega peliculas predefinidas
+                        },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Filled.Refresh,
+                                contentDescription = "Añadir 10 películas",
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        },
+                        text = {Text("Añadir 10 películas")}
+                    )
+
+                }
+                // opción para eliminar las peliculas seleccionadas solo si se activa la selección multiple
+                if(isActionMode.value){
+                    DropdownMenuItem(
+                        onClick = {
+                            // llamada a la funcion para eliminar las peliculas seleccionadas
+                            viewModel.deleteSelectedFilms(selectedFilms)
+                            // vacía la lista de las peliculas seleccionadas y desactiva el modo seleccion
+                            selectedFilms.clear()
+                            isActionMode.value = false
+                        },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Filled.Delete,
+                                contentDescription = "Borrar seleccionadas",
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        },
+                        text = {Text("Eliminar seleccionados")}
                     )
                 }
-            }
-
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = {expanded = false}
-            ) {
-                DropdownMenuItem(
-                    text = {Text("Acerca de")},
-                    onClick = {
-                        expanded = false
-                        onAbout?.invoke()
-                    }
-                )
-                DropdownMenuItem(
-                    text = {Text("Añadir película ")},
-                    onClick = {
-                        expanded = false
-                        onAddFilm?.invoke()
-                    }
-                )
             }
         }
-    )
+)
 }
