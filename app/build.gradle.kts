aea44630008a2aa7babac7e77212b5b5923ec9a6
Cristina ❤️ <mim0xina@gmail.com>
2026-05-02 19:53:51 +0200

    Actualizadas dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.campusdigitalfp.filmoteca"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
