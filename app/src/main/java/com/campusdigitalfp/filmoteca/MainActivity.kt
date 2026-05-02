5a55b2960f8126e1a26a2583fe4d2c4f4e66d39c
Cristina ❤️ <mim0xina@gmail.com>
2026-05-02 19:53:38 +0200

    Se añade authViewModel y se instancia en MainActivity.kt Actualiza Navigation Imports no utilizados

--- app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
@@ -1,48 +1,42 @@
 package com.campusdigitalfp.filmoteca
 
-import android.os.Build
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.viewmodel.compose.viewModel
-
 import com.campusdigitalfp.filmoteca.common.Logger
 import com.campusdigitalfp.filmoteca.navigation.Navigation
 import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
+import com.campusdigitalfp.filmoteca.viewmodel.AuthViewModel
 import com.campusdigitalfp.filmoteca.viewmodel.FilmViewModel
-import com.google.firebase.Firebase
-import  com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.firestore.MemoryCacheSettings
 
-
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inicialización de Firestore
+        // Inicialización de Firestore (solo RAM, sin caché persistente)
         val db = FirebaseFirestore.getInstance()
-
-        // Configura firestore sin almacenamiento en cache que persista
-        val settings = FirebaseFirestoreSettings.Builder().setLocalCacheSettings(MemoryCacheSettings.newBuilder()
-            .build()).build()
-        //utiliza solo RAM
-
-        db.firestoreSettings = settings //Se aplica la configuración a la db
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setLocalCacheSettings(MemoryCacheSettings.newBuilder().build())
+            .build()
+        db.firestoreSettings = settings
 
         enableEdgeToEdge()
+
         // Limpiar logs de ejecuciones antiguas
         Logger.clearLogs(this)
-        setContent {
-            val viewModel: FilmViewModel = viewModel()
 
-            //Se inicia la navegacion en la app y pasa el viewmodel
-            Navigation(viewModel)
+        setContent {
+            FilmotecaTheme {
+                val filmViewModel: FilmViewModel = viewModel()
+                val authViewModel: AuthViewModel = viewModel()
+
+                Navigation(viewModel = filmViewModel, authViewModel = authViewModel)
+            }
         }
     }
 }
-
